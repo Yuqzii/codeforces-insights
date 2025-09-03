a037cbc48e546d7504772f8d27f8669c9bfb1394
Yuqzii <p4r4yuqzii@gmail.com>
2025-09-03 20:07:18 +0200

    feat: add codeforces client and basic user request

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -4,12 +4,28 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/yuqzii/cf-stats/internal/codeforces"
 	"github.com/yuqzii/cf-stats/internal/transport"
+)
+
+const (
+	cfRequestsPerSecond float64 = 0.5
+	cfMaxBurst          int     = 1
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", transport.HandleRoot)
+
+	cfClient := codeforces.NewClient(
+		http.DefaultClient,
+		"https://codeforces.com/api/",
+		cfRequestsPerSecond,
+		cfMaxBurst)
+
+	h := transport.NewHandler(cfClient)
+
+	mux.HandleFunc("/", h.HandleRoot)
+	mux.HandleFunc("GET /users/{handle}", h.HandleGetUser)
 
 	fmt.Println("Server listening on port :8080")
 	http.ListenAndServe(":8080", mux)
--- backend/internal/transport/handlers.go
+++ backend/internal/transport/handlers.go
@@ -1,10 +1,44 @@
 package transport
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/yuqzii/cf-stats/internal/codeforces"
 )
 
-func HandleRoot(w http.ResponseWriter, r *http.Request) {
+type API interface {
+	GetUser(context.Context, string) (*codeforces.User, error)
+}
+
+type Handler struct {
+	api API
+}
+
+func NewHandler(api API) *Handler {
+	return &Handler{
+		api: api,
+	}
+}
+
+func (h *Handler) HandleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
+}
+
+func (h *Handler) HandleGetUser(w http.ResponseWriter, r *http.Request) {
+	handle := r.PathValue("handle")
+	user, err := h.api.GetUser(context.TODO(), handle)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	j, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
 }
