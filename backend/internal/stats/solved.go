8e60f6128601651b2d169d76728ee7b0bd0d2cbb
Yuqzii <p4r4yuqzii@gmail.com>
2025-09-05 17:09:48 +0200

    feat: add func to getting rating count of solved problems

--- backend/internal/stats/solved.go
+++ backend/internal/stats/solved.go
@@ -23,6 +23,21 @@ func (s *service) Categories(handle string) (map[string]int, error) {
 	return res, nil
 }
 
+// Gets the count of each rating of the user's solved problems
+func (s *service) Ratings(handle string) (map[int]int, error) {
+	sub, err := s.client.GetSubmissions(context.TODO(), handle)
+	if err != nil {
+		return nil, err
+	}
+
+	solved := filterSolved(sub)
+	res := make(map[int]int)
+	for _, s := range solved {
+		res[s.Problem.Rating]++
+	}
+	return res, nil
+}
+
 func filterSolved(sub []codeforces.Submission) []codeforces.Submission {
 	res := make([]codeforces.Submission, 0)
 	for _, s := range sub {
