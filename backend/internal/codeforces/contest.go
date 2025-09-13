5263bcc11a8b5fa53069887647aee372df42cdac
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-09-13 12:35:50 +0200

    feat: add function for getting contest standings

--- backend/internal/codeforces/contest.go
+++ backend/internal/codeforces/contest.go
@@ -1,8 +1,54 @@
 package codeforces
 
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/url"
+	"strconv"
+)
+
 type Contestant struct {
 	Rank    int     `json:"rank"`
 	Points  float64 `json:"points"`
 	Penalty int     `json:"penalty"`
 	Rating  int
+}
+
+var ErrNoStandings = errors.New("could not find standings")
+
+func (c *client) GetContestStandings(ctx context.Context, id int) ([]Contestant, error) {
+	endpoint := "contest.standings?"
+	params := url.Values{}
+	params.Set("contestId", strconv.Itoa(id))
+	params.Set("from", "1")
+	params.Set("showUnofficial", "false")
+
+	resp, err := c.makeRequest(ctx, "GET", endpoint+params.Encode())
+	if err != nil {
+		return nil, fmt.Errorf("getting contest standings from Codeforces: %w", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	type result struct {
+		Rows []Contestant `json:"rows"`
+	}
+	var apiResp apiResponse[result]
+	json.Unmarshal(body, &apiResp)
+
+	if apiResp.Status != "OK" {
+		return nil, fmt.Errorf("%w: %s", ErrCodeforcesReturnedFail, apiResp.Comment)
+	}
+
+	if len(apiResp.Result) == 0 {
+		return nil, ErrNoStandings
+	}
+
+	return apiResp.Result[0].Rows, nil
 }
