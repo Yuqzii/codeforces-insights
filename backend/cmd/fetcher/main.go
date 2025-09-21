35593476227631a44e6e00f11512bdca825f569f
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-09-21 19:33:34 +0200

    feat: add entrypoint for fetching contests to db

---
+++ backend/cmd/fetcher/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/yuqzii/cf-stats/internal/codeforces"
+	"github.com/yuqzii/cf-stats/internal/db"
+)
+
+const (
+	dbHost string = "postgres"
+	dbPort uint16 = 5432
+
+	cfRequestsPerSecond float64 = 0.5
+	cfMaxBurst          int     = 1
+)
+
+func main() {
+	log.Println("Connecting to database...")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPswd := os.Getenv("POSTGRES_PASSWORD")
+	db, err := db.New(context.Background(), dbHost, dbUser, dbPswd, dbName, dbPort)
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+	log.Println("Connected to database")
+	defer db.Close()
+
+	cfClient := codeforces.NewClient(
+		http.DefaultClient,
+		"https://codeforces.com/api/",
+		cfRequestsPerSecond,
+		cfMaxBurst,
+	)
+	_ = cfClient
+}
