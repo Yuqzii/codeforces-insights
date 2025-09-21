c43c3ce0c1cd2ae6e6301b74b1cb1caae3073fcd
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-09-21 13:43:02 +0200

    feat: set up database connection in go

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/yuqzii/cf-stats/internal/codeforces"
+	"github.com/yuqzii/cf-stats/internal/db"
 	"github.com/yuqzii/cf-stats/internal/transport"
 )
 
 const (
+	dbHost string = "postgres"
+	dbPort uint16 = 5432
+
 	cfRequestsPerSecond float64 = 0.5
 	cfMaxBurst          int     = 1
 )
 
 func main() {
-	mux := http.NewServeMux()
+	log.Println("Connecting to database...")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPswd := os.Getenv("POSTGRES_PASSWORD")
+	db, err := db.New(context.Background(), dbHost, dbUser, dbPswd, dbName, dbPort)
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+	log.Println("Connected to database.")
+	defer db.Close()
 
 	cfClient := codeforces.NewClient(
 		http.DefaultClient,
@@ -24,6 +40,7 @@ func main() {
 
 	h := transport.NewHandler(cfClient)
 
+	mux := http.NewServeMux()
 	mux.HandleFunc("/", h.HandleRoot)
 	mux.HandleFunc("GET /users/{handle}", h.HandleGetUser)
 	mux.HandleFunc("GET /users/solved-ratings/{handle}", h.HandleGetRatings)
