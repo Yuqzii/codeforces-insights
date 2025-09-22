07e8c5217b34ab60c975241e15aa834f2168124c
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-09-22 21:02:57 +0200

    feat: make UpsertUser return user_id

--- backend/internal/db/user.go
+++ backend/internal/db/user.go
@@ -2,11 +2,12 @@ package db
 
 import "context"
 
-func (db *db) UpsertUser(ctx context.Context, handle string, rating int) error {
-	_, err := db.conn.Exec(ctx, `
+func (db *db) UpsertUser(ctx context.Context, handle string, rating int) (user_id int, err error) {
+	err = db.conn.QueryRow(ctx, `
 		INSERT INTO users (handle, current_rating) VALUES ($1, $2)
 		ON CONFLICT (handle) DO UPDATE
 		SET current_rating = EXCLUDED.current_rating
-	`, handle, rating)
-	return err
+		RETURNING id`,
+		handle, rating).Scan(&user_id)
+	return user_id, err
 }
