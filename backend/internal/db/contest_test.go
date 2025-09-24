1ce5a3f06da18ec50166d621038a2246d0e438bc
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-09-24 19:26:20 +0200

    feat: add tests for db.ContestsExist

--- backend/internal/db/contest_test.go
+++ backend/internal/db/contest_test.go
@@ -30,7 +30,7 @@ func TestContestExists(t *testing.T) {
 			WillReturnRows(rows)
 
 		exists, err := db.ContestExists(ctx, 69)
-		assert.Nil(t, err)
+		assert.Nil(t, err, err)
 		assert.True(t, exists, "expected contest to exist")
 	})
 
@@ -41,7 +41,7 @@ func TestContestExists(t *testing.T) {
 			WillReturnRows(rows)
 
 		exists, err := db.ContestExists(ctx, 99)
-		assert.Nil(t, err)
+		assert.Nil(t, err, err)
 		assert.False(t, exists, "expected contest to not exist")
 	})
 
@@ -53,5 +53,69 @@ func TestContestExists(t *testing.T) {
 		exists, err := db.ContestExists(ctx, 13)
 		assert.NotNil(t, err, "expected error")
 		assert.False(t, exists, "expected exists=false on error")
+	})
+}
+
+func TestContestsExists(t *testing.T) {
+	ctx := context.Background()
+	mock, db := setupMockDB(t)
+
+	t.Run("All contests exist", func(t *testing.T) {
+		input := []int{1, 2, 50}
+		rows := pgxmock.NewRows([]string{"contest_id"}).
+			AddRow(1).AddRow(2).AddRow(50)
+		mock.ExpectQuery(`SELECT contest_id FROM contests WHERE contest_id = ANY\(\$1\)`).
+			WithArgs(input).
+			WillReturnRows(rows)
+
+		existing, err := db.ContestsExists(ctx, input)
+		assert.Nil(t, err, err)
+		for _, val := range input {
+			_, ok := existing[val]
+			assert.Truef(t, ok, "expected contest %d to exist", val)
+		}
+	})
+
+	t.Run("Some contests exist", func(t *testing.T) {
+		input := []int{42, 1, 69, 100}
+		shouldExist := []bool{true, false, true, false}
+		rows := pgxmock.NewRows([]string{"contest_id"}).
+			AddRow(42).AddRow(69)
+		mock.ExpectQuery(`SELECT contest_id FROM contests WHERE contest_id = ANY\(\$1\)`).
+			WithArgs(input).
+			WillReturnRows(rows)
+
+		existing, err := db.ContestsExists(ctx, input)
+		assert.Nil(t, err, err)
+		for i := range input {
+			_, ok := existing[input[i]]
+			assert.Equal(t, shouldExist[i], ok)
+		}
+	})
+
+	t.Run("No contests exist", func(t *testing.T) {
+		input := []int{10, 1337, 3, 69420}
+		rows := pgxmock.NewRows([]string{"contest_id"})
+		mock.ExpectQuery(`SELECT contest_id FROM contests WHERE contest_id = ANY\(\$1\)`).
+			WithArgs(input).
+			WillReturnRows(rows)
+
+		existing, err := db.ContestsExists(ctx, input)
+		assert.Nil(t, err, err)
+		for _, val := range input {
+			_, ok := existing[val]
+			assert.Falsef(t, ok, "did not expect contest %d to exist", val)
+		}
+	})
+
+	t.Run("Query error", func(t *testing.T) {
+		input := []int{10, 20}
+		mock.ExpectQuery(`SELECT contest_id FROM contests WHERE contest_id = ANY\(\$1\)`).
+			WithArgs(input).
+			WillReturnError(errors.New("query failed"))
+
+		existing, err := db.ContestsExists(ctx, input)
+		assert.NotNil(t, err, "expected error")
+		assert.Nilf(t, existing, "expected existing=nil on error, got %v", existing)
 	})
 }
