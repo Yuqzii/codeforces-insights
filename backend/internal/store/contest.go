f95a1ecd1ae2a8bcf7618d9de48089f843fd2027
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-09-27 13:05:09 +0200

    feat: add func to store for getting contest results

---
+++ backend/internal/store/contest.go
@@ -0,0 +1,54 @@
+package store
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/yuqzii/cf-stats/internal/codeforces"
+	"github.com/yuqzii/cf-stats/internal/db"
+)
+
+// First tries getting contest results from the DB, if unsuccessful tries the API.
+func (s *Store) GetContestResults(ctx context.Context, id int) (
+	[]codeforces.Contestant, *codeforces.Contest, error) {
+
+	contestants, contest, err := s.db.GetContestResults(ctx, id)
+	if err == nil {
+		return contestants, contest, nil
+	}
+
+	if !errors.Is(err, db.ErrContestNotStored) {
+		log.Printf("unexpected error querying db: %v\ntrying api", err)
+	}
+
+	contestants, contest, err = s.api.GetContestStandings(ctx, id)
+	if err != nil {
+		return nil, nil, fmt.Errorf("getting contest standings from api: %w", err)
+	}
+
+	ratings, err := s.api.GetContestRatingChanges(ctx, id)
+	if err != nil {
+		return nil, nil, fmt.Errorf("getting contest ratings from api: %w", err)
+	}
+
+	ratingMap := make(map[string]*codeforces.RatingChange)
+	for i := range ratings {
+		ratingMap[ratings[i].Handle] = &ratings[i]
+	}
+
+	// Set ratings of contestants
+	for i, c := range contestants {
+		for _, handle := range c.MemberHandles {
+			r, ok := ratingMap[handle]
+			// Use rating of party member with maximum previous rating
+			if ok && r.OldRating > contestants[i].OldRating {
+				contestants[i].OldRating = r.OldRating
+				contestants[i].NewRating = r.NewRating
+			}
+		}
+	}
+
+	return contestants, contest, nil
+}
