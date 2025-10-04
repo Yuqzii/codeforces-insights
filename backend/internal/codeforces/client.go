005ba4d0098c35fcfa9a8902ee6a3e8dc66e76a7
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-10-04 21:39:52 +0200

    feat: store requests in queue and receivers in map
    WIP, does not work in current state. Other code depending on this
    does not use the function correctly.

--- backend/internal/codeforces/client.go
+++ backend/internal/codeforces/client.go
@@ -3,7 +3,6 @@ package codeforces
 import (
 	"context"
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
 
@@ -13,30 +12,56 @@ import (
 var ErrCodeforcesReturnedFail = errors.New("Codeforces returned status FAILED") // nolint:staticcheck
 
 type client struct {
-	client  *http.Client
-	limiter *rate.Limiter
-	url     string
+	client    *http.Client
+	limiter   *rate.Limiter
+	url       string
+	requests  reqQueue
+	receivers map[string][]receiver
 }
 
 func NewClient(httpClient *http.Client, url string, requestsPerSecond float64, burst int) *client {
 	return &client{
-		client:  httpClient,
-		limiter: rate.NewLimiter(rate.Limit(requestsPerSecond), burst),
-		url:     url,
+		client:    httpClient,
+		limiter:   rate.NewLimiter(rate.Limit(requestsPerSecond), burst),
+		url:       url,
+		requests:  reqQueue{},
+		receivers: make(map[string][]receiver),
 	}
 }
 
-func (c *client) makeRequest(ctx context.Context, method, endpoint string) (*http.Response, error) {
-	if err := c.limiter.Wait(ctx); err != nil {
-		return nil, fmt.Errorf("waiting for limiter: %w", err)
+type receiver struct {
+	ctx context.Context
+	chn chan<- requestResult
+}
+
+type requestResult struct {
+	resp *http.Response
+	err  error
+}
+
+func (c *client) makeRequest(ctx context.Context, method, endpoint string) <-chan requestResult {
+	reqs, queued := c.receivers[endpoint]
+	if queued {
+		// Request is already queued, just add to receivers
+		respChan := make(chan requestResult)
+		reqs = append(reqs, receiver{
+			ctx: ctx,
+			chn: respChan,
+		})
+		return respChan
 	}
 
-	req, err := http.NewRequestWithContext(ctx, method, c.url+endpoint, nil)
-	if err != nil {
-		return nil, fmt.Errorf("creating request: %w", err)
-	}
+	// Push request to queue and create receiver list
+	c.requests.push(endpoint)
+	c.receivers[endpoint] = make([]receiver, 0, 1)
 
-	return c.client.Do(req)
+	respChan := make(chan requestResult)
+	c.receivers[endpoint] = append(c.receivers[endpoint], receiver{
+		ctx: ctx,
+		chn: respChan,
+	})
+
+	return respChan
 }
 
 type apiResponse[T any] struct {
