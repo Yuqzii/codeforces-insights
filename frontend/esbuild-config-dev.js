7f265b70bf90861c085c3c3eae9fa6e7a08f69c5
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-10-06 18:00:12 +0200

    fix: html not working with live reload
    Changed to using gaze instead with logic for retrying copy in case of
    failure. This issue only appeared when trying on Mac, not sure why.

--- frontend/esbuild-config-dev.js
+++ frontend/esbuild-config-dev.js
@@ -1,18 +1,45 @@
 esbuild = require("esbuild");
+gaze = require("gaze");
 fs = require("fs");
-path = require("path");
 
-function copyDir(srcDir, destDir) {
-	fs.mkdirSync(destDir, { recursive: true });
-	for (const entry of fs.readdirSync(srcDir, { withFileTypes: true })) {
-		const srcPath = path.join(srcDir, entry.name);
-		const destPath = path.join(destDir, entry.name);
-		if (entry.isDirectory())
-			copyDir(srcPath, destPath);
-		else
-			fs.copyFileSync(srcPath, destPath);
+gaze("./public/*", function(err, watcher) {
+	if (!fs.existsSync('/app/dist')) {
+		fs.mkdirSync('/app/dist', { recursive: true });
 	}
-}
+
+	// Copy all watched files into dist
+	var watched = this.watched();
+	for (const filepath of watched['/app/public/']) {
+		console.log("Copying", filepath);
+		var filename = filepath.replace(/^.*[\\/]/, '')
+		fs.copyFileSync(filepath, `/app/dist/${filename}`);
+	}
+
+	// On file changed
+	this.on('changed', function(filepath) {
+		console.log(filepath + ' was changed');
+		var filename = filepath.replace(/^.*[\\/]/, '')
+		var destPath = `/app/dist/${filename}`;
+
+		function tryCopy(attempts = 0) {
+			try {
+				if (!fs.existsSync('/app/dist')) {
+					fs.mkdirSync('/app/dist', { recursive: true });
+				}
+				fs.copyFileSync(filepath, destPath);
+			} catch (err) {
+				if (err.code === 'ENOENT' && attempts < 5) {
+					// Retry after a short delay
+					setTimeout(() => tryCopy(attempts + 1), attempts * 50);
+				} else {
+					console.error('Copy failed:', err);
+				}
+			}
+		}
+
+		tryCopy();
+	});
+});
 
 async function start() {
 	const ctx = await esbuild.context({
@@ -26,11 +53,6 @@ async function start() {
 
 	await ctx.watch();
 	console.log("[esbuild] Watching for changes");
-
-	copyDir("./public/", "./dist/");
-	const watcher = fs.watch("./public/", { recursive: true }, () => {
-		copyDir("./public/", "./dist/");
-	});
 
 	const shutdown = async () => {
 		console.log("Stopping esbuild");
