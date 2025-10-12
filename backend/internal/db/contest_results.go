311281a5a1e522de0de4d599b3f51dc664224faa
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-10-12 15:29:09 +0200

    feat: aggregate multiple result handles in db

--- backend/internal/db/contest_results.go
+++ backend/internal/db/contest_results.go
@@ -51,9 +51,10 @@ func (db *db) GetContestResultsTx(ctx context.Context, q Querier, id int) (
 	// Get contest
 	var contest codeforces.Contest
 	var internalID int
-	err := q.QueryRow(ctx,
-		`SELECT name, start_time, duration, id FROM contests WHERE contest_id=$1`, id).
-		Scan(&contest.Name, &contest.StartTime, &contest.Duration, &internalID)
+	err := q.QueryRow(ctx, `
+		SELECT name, start_time, duration, id FROM contests WHERE contest_id=$1`,
+		id,
+	).Scan(&contest.Name, &contest.StartTime, &contest.Duration, &internalID)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, nil, ErrContestNotStored
@@ -63,11 +64,19 @@ func (db *db) GetContestResultsTx(ctx context.Context, q Querier, id int) (
 
 	// Get contestants with handles
 	rows, err := q.Query(ctx, `
-		SELECT cr.rank, cr.old_rating, cr.new_rating, cr.points, cr.id, crh.handle
+		SELECT
+			cr.rank,
+			cr.old_rating,
+			cr.new_rating,
+			cr.points,
+			cr.id,
+			COALESCE(ARRAY_AGG(crh.handle) FILTER (WHERE crh.handle IS NOT NULL), '{}') AS handles
 		FROM contest_results AS cr
 		LEFT JOIN contest_result_handles crh ON crh.contest_result_id=cr.id
-		WHERE cr.contest_id=$1`,
-		internalID)
+		WHERE cr.contest_id = $1
+		GROUP BY cr.rank, cr.old_rating, cr.new_rating, cr.points, cr.id`,
+		internalID,
+	)
 	if err != nil {
 		return nil, nil, fmt.Errorf("querying contest_results: %w", err)
 	}
@@ -81,31 +90,18 @@ func (db *db) GetContestResultsTx(ctx context.Context, q Querier, id int) (
 }
 
 func scanToContestants(rows pgx.Rows) ([]codeforces.Contestant, error) {
-	contestantMap := make(map[int]codeforces.Contestant, 0)
-	var rank, oldRating, newRating, resultID int
-	var points float64
-	var handle string
-	_, err := pgx.ForEachRow(rows,
-		[]any{&rank, &oldRating, &newRating, &points, &resultID, &handle},
-		func() error {
-			c, ok := contestantMap[resultID]
-			if !ok {
-				contestantMap[resultID] = codeforces.Contestant{
-					Rank:      rank,
-					OldRating: oldRating,
-					NewRating: newRating,
-					Points:    points,
-				}
-			}
-			c.MemberHandles = append(c.MemberHandles, handle)
+	return pgx.CollectRows(rows, func(row pgx.CollectableRow) (codeforces.Contestant, error) {
+		var c codeforces.Contestant
 
-			return nil
-		})
+		err := row.Scan(
+			&c.Rank,
+			&c.OldRating,
+			&c.NewRating,
+			&c.Points,
+			&c.ID,
+			&c.MemberHandles,
+		)
 
-	contestantSlice := make([]codeforces.Contestant, 0, len(contestantMap))
-	for _, c := range contestantMap {
-		contestantSlice = append(contestantSlice, c)
-	}
-
-	return contestantSlice, err
+		return c, err
+	})
 }
