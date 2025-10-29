aee0d79543072f9e02b4ba7087e271f075e93e97
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-10-29 10:49:29 +0100

    feat: add func for getting percentile

--- backend/internal/stats/percentile.go
+++ backend/internal/stats/percentile.go
@@ -4,13 +4,13 @@ import (
 	"github.com/yuqzii/cf-stats/internal/codeforces"
 )
 
-type percentileCalc struct {
+type PercentileCalc struct {
 	prefix []int
 }
 
-func NewPercentile(users []codeforces.User) *percentileCalc {
-	p := &percentileCalc{
-		prefix: make([]int, maxRating),
+func NewPercentile(users []codeforces.User) *PercentileCalc {
+	p := &PercentileCalc{
+		prefix: make([]int, maxRating+1),
 	}
 
 	for _, user := range users {
@@ -21,4 +21,11 @@ func NewPercentile(users []codeforces.User) *percentileCalc {
 	}
 
 	return p
+}
+
+func (p *PercentileCalc) GetPercentile(rating int) float64 {
+	if rating > maxRating || rating < 0 {
+		return 0
+	}
+	return float64(p.prefix[rating]) / float64(p.prefix[maxRating])
 }
