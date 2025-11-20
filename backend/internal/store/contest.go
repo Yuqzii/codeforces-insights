6c0dcca70eccbb8d464146d7e8922d74f34ecb8c
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2025-11-20 18:58:09 +0100

    feat: add contest results from handle func to store

--- backend/internal/store/contest.go
+++ backend/internal/store/contest.go
@@ -42,6 +42,43 @@ func (s *Store) GetContestResults(ctx context.Context, id int) (
 	return contestants, contest, nil
 }
 
+// First tries the DB, if unsuccessful tries the API.
+// NOTE: can return partially incomplete Contestant data,
+// as the Codeforces API does not return all the information we have stored.
+func (s *Store) GetContestResultsFromHandle(ctx context.Context, handle string) (
+	[]codeforces.Contestant, error) {
+
+	contestants, err := s.db.GetContestResultsFromHandle(ctx, handle)
+	if err == nil {
+		return contestants, err
+	}
+
+	if errors.Is(err, context.Canceled) {
+		return nil, err
+	}
+
+	if !errors.Is(err, db.ErrNoResultsWithHandle) {
+		log.Printf("unexpected error querying db for contest results from handle: %v\ntrying api", err)
+	}
+
+	ratings, err := s.api.GetRatingChanges(ctx, handle)
+	if err != nil {
+		return nil, fmt.Errorf("getting rating changes for handle '%s' from api:, %w", handle, err)
+	}
+
+	contestants = make([]codeforces.Contestant, 0, len(ratings))
+	for _, r := range ratings {
+		contestants = append(contestants, codeforces.Contestant{
+			Rank:          r.Rank,
+			OldRating:     r.OldRating,
+			NewRating:     r.NewRating,
+			MemberHandles: []string{r.Handle},
+		})
+	}
+
+	return contestants, nil
+}
+
 // Adds the correct rating to each contestant. (Changes the provided contestants).
 func MapRatingToContestants(ratings []codeforces.RatingChange, contestants []codeforces.Contestant) {
 	ratingMap := make(map[string]*codeforces.RatingChange)
