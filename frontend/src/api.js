3bb2c342799a97ef5716a9bd98c484f6e4a38d27
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-01-05 09:26:14 +0100

    feat: add func for getting cf problems from tags
    Closes #145

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,6 +10,7 @@ async function cfFetch(endpoint, signal) {
 	} catch (err) {
 		if (err.name === "AbortError") return;
 		console.error("Codeforces request failed:", err);
+		throw err;
 	}
 }
 
@@ -24,6 +25,19 @@ export async function getSubmissions(handle, signal) {
 
 export async function getRatingHistory(handle, signal) {
 	return await cfFetch(`user.rating?handle=${handle}`, signal);
+}
+
+export async function getProblems(tags, signal) {
+	let url = "problemset.problems";
+	if (tags) {
+		url += "?tags=";
+		for (const tag of tags) {
+			url += tag + ";";
+		}
+		url.slice(0, -1);
+	}
+
+	return await cfFetch(url, signal);
 }
 
 export async function getPerformance(handle, ratingHistory, signal) {
