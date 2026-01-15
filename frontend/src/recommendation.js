d01e8db264b48361ee7ede4094341823746194ca
MartinGjuvsland <132813821+MartinGjuvsland@users.noreply.github.com>
2026-01-15 18:39:09 +0100

    feat: add function filterSubissionsRecentContests (#147)
    Closes #143 
    Filters all submissions from a users five most recent contests.

---
+++ frontend/src/recommendation.js
@@ -0,0 +1,22 @@
+export function filterSubmissionsRecentContests(submissions) {
+    const filtered = [];
+    const recentContests = [];
+    
+    for (let i = 0; i < submissions.length; i++){
+        if (submissions[i].author.participantType != "CONTESTANT") {
+            continue;
+        }
+
+        let amountOfContests=5;
+        let ID=submissions[i].contestId;
+        if (recentContests.includes(ID)) {
+            filtered[recentContests.indexOf(ID)].push(submissions[i]);
+        } else if (recentContests.length < amountOfContests) {
+            filtered.push([submissions[i]]);
+            recentContests.push(ID);
+        }
+
+    }
+    
+    return filtered
+}
