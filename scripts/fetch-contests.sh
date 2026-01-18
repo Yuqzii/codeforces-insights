3133a504d7d2345af34cb3addfd05b6f533f6376
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-01-18 18:14:08 +0100

    fix: cd into project dir when running fetch-contests.sh (#152)
    Closes #151 
    Also add `--remove-orphans` to remove orphan containers when running the
    script.

--- scripts/fetch-contests.sh
+++ scripts/fetch-contests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-docker compose run --build fetcher
+set -euo pipefail
+
+cd "$(dirname "$0")/.."
+
+docker compose run --build fetcher --remove-orphans
 
 echo "Cleaning up old images..."
 docker image prune -f
