70d1f0c2577043aa966c4513a655606f4b46376e
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-01-23 17:25:02 +0100

    feat: create priority queue for problems

---
+++ backend/internal/recommender/priority_queue.go
@@ -0,0 +1,38 @@
+package recommender
+
+import "github.com/yuqzii/cf-stats/internal/codeforces"
+
+type probWithScore struct {
+	score   float64
+	problem *codeforces.Problem
+	index   int
+}
+
+type probPQ []*probWithScore
+
+func (pq probPQ) Len() int {
+	return len(pq)
+}
+
+func (pq probPQ) Less(i, j int) bool {
+	return pq[i].score < pq[j].score
+}
+
+func (pq probPQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *probPQ) Push(x any) {
+	p := x.(*probWithScore)
+	p.index = len(*pq)
+	*pq = append(*pq, p)
+}
+
+func (pq *probPQ) Pop() any {
+	last := (*pq)[len(*pq)-1]
+	(*pq)[len(*pq)-1] = nil
+	*pq = (*pq)[0 : len(*pq)-1]
+	return last
+}
