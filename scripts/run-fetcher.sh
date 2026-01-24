3409b099b75ca495ff49d211c09bc727aafbd042
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-01-24 14:58:02 +0100

    fix: update and rename fetcher script for cli arguments

--- scripts/run-fetcher.sh
+++ scripts/run-fetcher.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 cd "$(dirname "$0")/.."
 
-docker compose run --build fetcher --remove-orphans
+docker compose run --build --rm --remove-orphans fetcher "$@" # "$@" forwards all arguments.
 
 echo "Cleaning up old images..."
 docker image prune -f
