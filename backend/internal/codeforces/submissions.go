ddecf4f7b7b384cb19abf46526974789e8f32784
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-01-24 12:27:05 +0100

    refactor: add separate func for hashing prob with id and index
    Allows generating a problem hash without constructing a full Problem
    object beforehand.

--- backend/internal/codeforces/submissions.go
+++ backend/internal/codeforces/submissions.go
@@ -59,12 +59,16 @@ func (c *client) GetSubmissions(ctx context.Context, handle string) ([]Submissio
 }
 
 func (p *Problem) Hash() int64 {
-	var res int64
-	res = int64(p.ContestID) << 32
-	res |= 1 << (p.Index[0] - 'A' + 2) // One bit for indicating the character
+	return HashProblemIndex(p.ContestID, p.Index)
+}
 
-	if len(p.Index) > 1 {
-		res |= int64(p.Index[1] - '1') // Last two bits indicating number
+func HashProblemIndex(contestID int, index string) int64 {
+	var res int64
+	res = int64(contestID) << 32
+	res |= 1 << (index[0] - 'A' + 2) // One bit for indicating the character
+
+	if len(index) > 1 {
+		res |= int64(index[1] - '1') // Last two bits indicating number
 	}
 
 	return res
