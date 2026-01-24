523c212857f3e98579e4f17740b1e5e910eafaa8
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-01-24 13:40:53 +0100

    feat: add function to get problems with tags from db

---
+++ backend/internal/db/problems.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/yuqzii/cf-stats/internal/codeforces"
+)
+
+func (db *db) GetProblemsWithTags(ctx context.Context, tags []string) ([]codeforces.Problem, error) {
+	return db.GetProblemsWithTagsTx(ctx, db.q, tags)
+}
+
+func (db *db) GetProblemsWithTagsTx(ctx context.Context, q Querier, tags []string) ([]codeforces.Problem, error) {
+	rows, err := q.Query(ctx, `
+		SELECT
+			p.name,
+			p.index,
+			p.rating,
+			p.tags,
+			c.contest_id
+		FROM problems p
+		JOIN contests c ON p.contest_id = c.id
+		WHERE p.tags && $1`,
+		tags,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("querying problems: %w", err)
+	}
+
+	problems, err := pgx.CollectRows(rows, pgx.RowToStructByName[codeforces.Problem])
+	if err != nil {
+		return nil, fmt.Errorf("collecting rows: %w", err)
+	}
+
+	return problems, nil
+}
