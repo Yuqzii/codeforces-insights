d26359a8c7be2e8a01a5d728cd31b7ec42d79ff2
Martin Gjuvsland <gjuvslandmartin@gmail.com>
2026-02-02 08:42:02 +0100

    removed unnecessary variable "filtered"

--- frontend/src/recommendation.js
+++ frontend/src/recommendation.js
@@ -1,7 +1,6 @@
 export function filterSubmissionsRecentContests(submissions,amountOfContests) {
-    const filtered = [];
+    const recentContestsId = [];
     const recentContests = [];
-    const returnObject = [];
 
     for (let i = 0; i < submissions.length; i++){
         if (submissions[i].author.participantType != "CONTESTANT") {
@@ -9,19 +8,15 @@ export function filterSubmissionsRecentContests(submissions,amountOfContests) {
         }
 
         let ID=submissions[i].contestId;
-        if (recentContests.includes(ID)) {
-            filtered[recentContests.indexOf(ID)].push(submissions[i]);
-        } else if (recentContests.length < amountOfContests) {
-            filtered.push([submissions[i]]);
-            recentContests.push(ID);
+        if (recentContestsId.includes(ID)) {
+            recentContests[recentContestsId.indexOf(ID)].submissions.push(submissions[i]);
+        } else if (recentContestsId.length < amountOfContests) {
+            recentContestsId.push(ID);
+            recentContests.push({id: ID, submissions: [submissions[i]]})
         }
     }
 
-    for (let i = 0; i < recentContests.length; i++) {
-        returnObject.push({id: recentContests[i], submissions: filtered[i]})
-    }
-
-    return  returnObject
+    return  recentContests
 }
 
 export function findSolvedProblemsRecentContests(contests) {
@@ -31,12 +26,12 @@ export function findSolvedProblemsRecentContests(contests) {
         const solvedInContest = [];
 
         for (const problem of contest.submissions) {
-            if (problem.verdict=="OK" && !(solvedInContest.includes(problem.index))) {
+            if (problem.verdict == "OK" && !(solvedInContest.includes(problem.index))) {
                 solvedInContest.push(problem.problem.index);
             }
         }
 
-        solvedByContests.push({id:contest.id, indices:solvedInContest})
+        solvedByContests.push({id: contest.id, indices: solvedInContest})
     }
 
     return solvedByContests
