dc0179ebc52c0a0a386a7ecf82c9a92f2aa0382a
MartinGjuvsland <132813821+MartinGjuvsland@users.noreply.github.com>
2026-02-03 20:10:15 +0100

    feat: find solved problem-indices from each recent contest and add CF hyperlink (#163)
    Closes #144 
    Closes #164 
    Added the `findSolvedProblemsRecentContests` function which returns an
    array of objects containing a contest-id and an array of
    problem-indices. Turned the username element into Codeforces hyperlink

--- frontend/src/recommendation.js
+++ frontend/src/recommendation.js
@@ -1,22 +1,38 @@
-export function filterSubmissionsRecentContests(submissions) {
-    const filtered = [];
+export function filterSubmissionsRecentContests(submissions,amountOfContests) {
+    const recentContestsId = [];
     const recentContests = [];
-    
+
     for (let i = 0; i < submissions.length; i++){
         if (submissions[i].author.participantType != "CONTESTANT") {
             continue;
         }
 
-        let amountOfContests=5;
         let ID=submissions[i].contestId;
-        if (recentContests.includes(ID)) {
-            filtered[recentContests.indexOf(ID)].push(submissions[i]);
-        } else if (recentContests.length < amountOfContests) {
-            filtered.push([submissions[i]]);
-            recentContests.push(ID);
+        if (recentContestsId.includes(ID)) {
+            recentContests[recentContestsId.indexOf(ID)].submissions.push(submissions[i]);
+        } else if (recentContestsId.length < amountOfContests) {
+            recentContestsId.push(ID);
+            recentContests.push({id: ID, submissions: [submissions[i]]})
+        }
+    }
+
+    return  recentContests
+}
+
+export function findSolvedProblemsRecentContests(contests) {
+    const solvedByContests = [];
+
+    for (const contest of contests) {
+        const solvedInContest = [];
+
+        for (const problem of contest.submissions) {
+            if (problem.verdict == "OK" && !(solvedInContest.includes(problem.index))) {
+                solvedInContest.push(problem.problem.index);
+            }
         }
 
+        solvedByContests.push({id: contest.id, indices: solvedInContest})
     }
-    
-    return filtered
+
+    return solvedByContests
 }
