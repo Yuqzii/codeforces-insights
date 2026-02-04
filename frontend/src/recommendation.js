ed3df7c0c2bb80b34eb172fc684be95d4f43540b
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-02-04 08:43:01 +0100

    feat: add func to update colors of the problem ratings
    Sets the color to the color of the corresponding Codefores rank.

--- frontend/src/recommendation.js
+++ frontend/src/recommendation.js
@@ -1,42 +1,42 @@
 import { getRatingColor } from "./charts";
 
-export function filterSubmissionsRecentContests(submissions,amountOfContests) {
-    const recentContestsId = [];
-    const recentContests = [];
+export function filterSubmissionsRecentContests(submissions, amountOfContests) {
+	const recentContestsId = [];
+	const recentContests = [];
 
-    for (let i = 0; i < submissions.length; i++){
-        if (submissions[i].author.participantType != "CONTESTANT") {
-            continue;
-        }
+	for (let i = 0; i < submissions.length; i++) {
+		if (submissions[i].author.participantType != "CONTESTANT") {
+			continue;
+		}
 
-        let ID=submissions[i].contestId;
-        if (recentContestsId.includes(ID)) {
-            recentContests[recentContestsId.indexOf(ID)].submissions.push(submissions[i]);
-        } else if (recentContestsId.length < amountOfContests) {
-            recentContestsId.push(ID);
-            recentContests.push({id: ID, submissions: [submissions[i]]})
-        }
-    }
+		let ID = submissions[i].contestId;
+		if (recentContestsId.includes(ID)) {
+			recentContests[recentContestsId.indexOf(ID)].submissions.push(submissions[i]);
+		} else if (recentContestsId.length < amountOfContests) {
+			recentContestsId.push(ID);
+			recentContests.push({ id: ID, submissions: [submissions[i]] })
+		}
+	}
 
-    return  recentContests
+	return recentContests
 }
 
 export function findSolvedProblemsRecentContests(contests) {
-    const solvedByContests = [];
+	const solvedByContests = [];
 
-    for (const contest of contests) {
-        const solvedInContest = [];
+	for (const contest of contests) {
+		const solvedInContest = [];
 
-        for (const problem of contest.submissions) {
-            if (problem.verdict == "OK" && !(solvedInContest.includes(problem.index))) {
-                solvedInContest.push(problem.problem.index);
-            }
-        }
+		for (const problem of contest.submissions) {
+			if (problem.verdict == "OK" && !(solvedInContest.includes(problem.index))) {
+				solvedInContest.push(problem.problem.index);
+			}
+		}
 
-        solvedByContests.push({id: contest.id, indices: solvedInContest})
-    }
+		solvedByContests.push({ id: contest.id, indices: solvedInContest })
+	}
 
-    return solvedByContests
+	return solvedByContests
 }
 
 // Sets the color of all problem rating elements to their corresponding Codeforces rank color.
