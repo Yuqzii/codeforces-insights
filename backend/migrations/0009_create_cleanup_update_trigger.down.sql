674bb72cd75642a3f3a67727dfb9ab702b406449
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-02-16 20:40:53 +0100

    fix: make migration 0009 down work

--- backend/migrations/0009_create_cleanup_update_trigger.down.sql
+++ backend/migrations/0009_create_cleanup_update_trigger.down.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
+DROP TRIGGER IF EXISTS delete_contest_dependents_on_update ON contests;
 DROP FUNCTION IF EXISTS delete_contest_dependents;
-DROP TRIGGER IF EXISTS delete_contest_dependents_on_update;
 
 COMMIT;
