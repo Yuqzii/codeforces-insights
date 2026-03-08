11911093df025068ae5497419639c75c4d294961
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-03-08 12:24:20 +0100

    fix: allow Content-Type header CORS

--- backend/internal/handlers/cors.go
+++ backend/internal/handlers/cors.go
@@ -6,7 +6,7 @@ func WithCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "https://cf-insights.org")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Add("Access-Control-Allow-Headers", "Content-Encoding")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Encoding, Content-Type")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
