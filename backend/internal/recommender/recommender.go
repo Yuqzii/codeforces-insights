0bd6ef9f33953c4ab3df5f416d6337ef563a94bc
Torstein Barvik Molstad <tosteinmols@gmail.com>
2026-05-04 22:38:48 +0200

    add weight based on how new a problem is, and gave *special problems negative score

--- backend/internal/recommender/recommender.go
+++ backend/internal/recommender/recommender.go
@@ -79,7 +79,7 @@ func (r *recommender) Recommend(ctx context.Context, probs []*codeforces.Problem
 		}
 
 		v := r.problemToVec(&prob)
-		score := similarity(&u, v)
+		score := similarity(&u, v)*(p.contest_id/1500) //simple scaling to push newer problems
 
 		ps := ProbWithScore{
 			Score:   score,
